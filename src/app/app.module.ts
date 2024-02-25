3ebbbd409c10abb1a96b04ca175c3d8039d74e0d
Gonzalo Sánchez <gonzafull14@gmail.com>
2024-02-25 18:40:29 -0300

    Instaling Tailwind and generate components Login and Sing-Up. Also, generate new user service. Routing Login as a Index to test Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
